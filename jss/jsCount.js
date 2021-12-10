e7fdc729b59e4f07565f74474b75f5da4c590a54
ozmarshall <parc.tec20@yahoo.com>
2021-12-09 22:45:21 -0500

    rem

--- jss/jsCount.js
+++ jss/jsCount.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
     year: 2021, // required
     month: 12, // required
-    day: 9, // required
+    day: 10, // required
     hours: 19, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
